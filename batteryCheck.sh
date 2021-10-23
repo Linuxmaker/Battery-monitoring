05c7fc5fe948817491fe05a800b9ce2a32b4c7cf
Andreas Günther <Linuxmaker@users.noreply.github.com>
2021-10-23 13:00:55 +0200

    Add files via upload

---
+++ batteryCheck.sh
@@ -0,0 +1,12 @@
+# /bin/bash
+
+BATTERY=`acpi | awk '{print $4}' | cut -c 1-2`
+STATUS=`acpi | awk '{print $3}'| cut -c 1-4`
+if [[ $BATTERY -ge 80 ]] && [[ $STATUS == "Char"  ]]
+then
+	notify-send --urgency=critical  --expire-time=5000  -i /usr/share/icons/Adwaita/256x256/legacy/battery-full.pn "Battery is loaded with $BATTERY%!" "The laptop can be taken from the charging current!"
+elif [[ $BATTERY -le 40 ]] && [[ $STATUS == "Disc"  ]]
+then	
+	notify-send --urgency=critical  --expire-time=5000  -i /usr/share/icons/Adwaita/256x256/legacy/battery-caution.png "Battery has reached $BATTERY%!" "The laptop must be connected to the charging current!"
+fi
+exit
