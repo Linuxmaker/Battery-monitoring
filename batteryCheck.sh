924b7215e56ce51cbb8b0d591d782adf880704ed
Andreas Günther <Linuxmaker@users.noreply.github.com>
2021-10-24 10:48:15 +0200

    Update batteryCheck.sh
    The query for the $ XDG_CURRENT_DESKTOP variable has been added because the KDE will not execute the script via cronjob for security reasons as long as this code has not been executed.

--- batteryCheck.sh
+++ batteryCheck.sh
@@ -1,12 +1,24 @@
-#!/bin/bash
+# /bin/bash
 
 BATTERY=`acpi | awk '{print $4}' | cut -c 1-2`
 STATUS=`acpi | awk '{print $3}'| cut -c 1-4`
+
+# Necessary for KDE because this desktop uses a security feature  
+# --- Begin KDE ---
+if [ $XDG_CURRENT_DESKTOP == "KDE" ]
+then
+        username=$(/usr/bin/whoami)
+        pid=$(ps -u $username e | grep -m 1 DBUS_SESSION_BUS_ADDRESS | awk '{print $1}')
+        dbus=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$pid/environ | sed 's/DBUS_SESSION_BUS_ADDRESS=//')
+        export DBUS_SESSION_BUS_ADDRESS=$dbus
+fi
+# --- End KDE ---
+
 if [[ $BATTERY -ge 80 ]] && [[ $STATUS == "Char"  ]]
 then
-	DISPLAY=:0 notify-send --urgency=critical  --expire-time=5000  -i /usr/share/icons/Adwaita/256x256/legacy/battery-full.pn "Battery is loaded with $BATTERY%!" "The laptop can be taken from the charging current!"
+        DISPLAY=:0 && notify-send --urgency=critical  --expire-time=5000  -i /usr/share/icons/Adwaita/256x256/legacy/battery-full.pn "Battery is loaded with $BATTERY%!" "The laptop can be taken from the charging current!"
 elif [[ $BATTERY -le 40 ]] && [[ $STATUS == "Disc"  ]]
-then	
-	DISPLAY=:0 notify-send --urgency=critical  --expire-time=5000  -i /usr/share/icons/Adwaita/256x256/legacy/battery-caution.png "Battery has reached $BATTERY%!" "The laptop must be connected to the charging current!"
+then
+        DISPLAY=:0 && notify-send --urgency=critical  --expire-time=5000  -i /usr/share/icons/Adwaita/256x256/legacy/battery-caution.png "Battery has reached $BATTERY%!" "The laptop must be connected to the charging current!"
 fi
 exit
