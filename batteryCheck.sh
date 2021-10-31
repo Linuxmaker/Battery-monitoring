482d0c006c425e2be121bbb58c76287170ccc441
Andreas Günther <Linuxmaker@users.noreply.github.com>
2021-10-31 19:17:55 +0100

    Update batteryCheck.sh
    Without "tr '\0' '\n' |" at line 16 we will get the message "Warning: Command replacement: NULL byte ignored in the input." and the script doesn't run.

--- batteryCheck.sh
+++ batteryCheck.sh
@@ -13,7 +13,7 @@ if [ $XDG_CURRENT_DESKTOP == "KDE" ]
 then
         username=$(/usr/bin/whoami)
         pid=$(ps -u $username e | grep -m 1 DBUS_SESSION_BUS_ADDRESS | awk '{print $1}')
-        dbus=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$pid/environ | sed 's/DBUS_SESSION_BUS_ADDRESS=//')
+        dbus=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$pid/environ | tr '\0' '\n' | sed 's/DBUS_SESSION_BUS_ADDRESS=//')
         export DBUS_SESSION_BUS_ADDRESS=$dbus
 fi
 # --- End KDE ---
