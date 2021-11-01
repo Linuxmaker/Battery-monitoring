e41316736bcb1ac5f1219d735a5978ae095beb3a
Andreas Günther <Linuxmaker@users.noreply.github.com>
2021-11-01 10:25:17 +0100

    Update batteryCheck.sh
    Additional warning feature for critical state of charge less than 20% and greater than 85%, which is not good for batteries.

--- batteryCheck.sh
+++ batteryCheck.sh
@@ -1,15 +1,19 @@
-# /bin/bash
+#! /bin/bash
 
 BATTERY=`acpi | awk '{print $4}' | cut -c 1-2`
 STATUS=`acpi | awk '{print $3}'| cut -c 1-4`
 
-#Recommended values are between 30% and 75% at laptops batteries
+# Recommended values are between 30% and 75% at laptops batteries
 MIN=30 
 MAX=75
 
+# In the long term, the state of charge should be avoided below 20% and above 85%. 
+MMIN=20
+MMAX=85
+
 # Necessary for KDE because this desktop uses a security feature  
 # --- Begin KDE ---
-if [ $XDG_CURRENT_DESKTOP == "KDE" ]
+if [ "$XDG_CURRENT_DESKTOP" = 'KDE' ]
 then
         username=$(/usr/bin/whoami)
         pid=$(ps -u $username e | grep -m 1 DBUS_SESSION_BUS_ADDRESS | awk '{print $1}')
@@ -18,11 +22,19 @@ then
 fi
 # --- End KDE ---
 
-if [[ $BATTERY -ge $MAX ]] && [[ $STATUS == "Char"  ]]
+if [ $BATTERY -ge $MAX ] && [ "$STATUS" = 'Char' ]
 then
-        DISPLAY=:0 && notify-send --urgency=critical  --expire-time=5000  -i /usr/share/icons/Adwaita/256x256/legacy/battery-full.pn "Battery is loaded with $BATTERY%!" "The laptop can be taken from the charging current!"
-elif [[ $BATTERY -le $MIN ]] && [[ $STATUS == "Disc"  ]]
+        DISPLAY=:0 && notify-send --urgency=critical  --expire-time=5000  -i /usr/share/icons/Adwaita/256x256/legacy/battery-full.png "Battery is loaded with $BATTERY%!" "Stop the charging process as the battery is leaving its optimal state of charge."
+elif [ $BATTERY -le $MIN ] && [ "$STATUS" = 'Disc' ]
 then
-        DISPLAY=:0 && notify-send --urgency=critical  --expire-time=5000  -i /usr/share/icons/Adwaita/256x256/legacy/battery-caution.png "Battery has reached $BATTERY%!" "The laptop must be connected to the charging current!"
+        DISPLAY=:0 && notify-send --urgency=critical  --expire-time=5000  -i /usr/share/icons/Adwaita/256x256/legacy/battery-caution-charging.png "Battery has reached $BATTERY%!" "Start the charging process as the battery has reached the optimum state of charge."
+fi
+
+if [ $BATTERY -ge $MMAX ] && [ "$STATUS" = 'Char' ]
+then
+        DISPLAY=:0 && notify-send --urgency=critical  --expire-time=5000  -i /usr/share/icons/Adwaita/256x256/legacy/battery-full-charging.png "Battery is loaded with $BATTERY%!" "Stop the charging process immediately to save the battery!"
+elif [ $BATTERY -le $MMIN ] && [ "$STATUS" = 'Disc' ]
+then
+        DISPLAY=:0 && notify-send --urgency=critical  --expire-time=5000  -i /usr/share/icons/Adwaita/256x256/legacy/battery-caution.png "Battery has reached $BATTERY%!" "Start the charging process immediately to save the battery!"
 fi
 exit
