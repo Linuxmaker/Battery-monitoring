8f18d11965792e4478f53fbbe7fb82761b4f61a1
Andreas Günther <Linuxmaker@users.noreply.github.com>
2022-05-12 16:29:35 +0200

    Update batteryCheck.sh
    The XDG_CURRENT_DESKTOP variable doesn't work in crond, so the IF query doesn't run on KDE. However, it doesn't do any harm if these four commands are run under a different desktop.

--- batteryCheck.sh
+++ batteryCheck.sh
@@ -13,13 +13,10 @@ MMAX=85
 
 # Necessary for KDE because this desktop uses a security feature  
 # --- Begin KDE ---
-if [ "$XDG_CURRENT_DESKTOP" = 'KDE' ]
-then
-        username=$(/usr/bin/whoami)
-        pid=$(ps -u $username e | grep -m 1 DBUS_SESSION_BUS_ADDRESS | awk '{print $1}')
-        dbus=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$pid/environ | tr '\0' '\n' | sed 's/DBUS_SESSION_BUS_ADDRESS=//')
-        export DBUS_SESSION_BUS_ADDRESS=$dbus
-fi
+username=$(/usr/bin/whoami)
+pid=$(ps -u $username e | grep -m 1 DBUS_SESSION_BUS_ADDRESS | awk '{print $1}')
+dbus=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$pid/environ | tr '\0' '\n' | sed 's/DBUS_SESSION_BUS_ADDRESS=//')
+export DBUS_SESSION_BUS_ADDRESS=$dbus
 # --- End KDE ---
 
 if [ $BATTERY -ge $MAX ] && [ "$STATUS" = 'Char' ]
